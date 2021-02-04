293e9a895fde7cea95b61612301743a75bc3507c
alyslma <alyssalma@gmail.com>
2021-02-04 10:54:23 -0500

    add contest leaderboard exercise

--- SQL/MediumBasicJoins.sql
+++ SQL/MediumBasicJoins.sql
@@ -130,3 +130,54 @@ HAVING COUNT(h.hacker_id) > 1
 ORDER BY COUNT(h.hacker_id) DESC, h.hacker_id;
 
 
+
+----------------+---------------
+--    CONTEST LEADERBOARD     --
+----------------+---------------
+-- https://www.hackerrank.com/challenges/contest-leaderboard --
+
+/* The tables HACKERS and SUBMISSIONS are described as follows:
+
+The hacker_id is the id of the hacker, and name is the name of the hacker.
++-------------+-------------+
+|          HACKERS          |
++-------------+-------------+
+|  Column     |    Type     |
++-------------+-------------+
+| hacker_id   | Integer     |
+| name        | String      |
++-------------+-------------+
+
+The submission_id is the id of the submission, 
+hacker_id is the id of the hacker who made the submission, 
+challenge_id is the id of the challenge for which the submission belongs to, 
+and score is the score of the submission.
++-------------------+------------------+
+|              SUBMISSIONS             |
++-------------------+------------------+
+|       Column      |       Type       |
++-------------------+------------------+
+|  submission_id    |      Integer     |
+|  hacker_id        |      Integer     |
+|  challenge_id     |      Integer     |
+|  score            |      Integer     |
++-------------------+------------------+
+
+You did such a great job helping Julia with her last coding contest challenge that she wants you to work on this one, too!
+
+The total score of a hacker is the sum of their maximum scores for all of the challenges. 
+Write a query to print the hacker_id, name, and total score of the hackers ordered by the descending score. 
+If more than one hacker achieved the same total score, then sort the result by ascending hacker_id. 
+Exclude all hackers with a total score of 0 from your result.
+*/
+
+-- Using aliases/subqueries and max() as some hackers can submit multiple submissions of the same challenge.
+SELECT h.hacker_id, h.name, sum(score)
+FROM HACKERS h
+JOIN (SELECT hacker_id, max(score) AS score FROM SUBMISSIONS GROUP BY challenge_id, hacker_id) max_score -- temp table
+ON h.hacker_id = max_score.hacker_id
+GROUP BY h.hacker_id, h.name
+HAVING sum(score) > 0
+ORDER BY sum(score) DESC, h.hacker_id;
+
+
