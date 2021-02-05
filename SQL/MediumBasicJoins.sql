f397b6d6329c848c73fef960d2d3326102ca4b84
alyslma <alyssalma@gmail.com>
2021-02-05 11:35:58 -0500

    add ollivander's inventory exercise

--- SQL/MediumBasicJoins.sql
+++ SQL/MediumBasicJoins.sql
@@ -181,3 +181,80 @@ HAVING sum(score) > 0
 ORDER BY sum(score) DESC, h.hacker_id;
 
 
+
+
+----------------+---------------
+--   OLLIVANDER'S INVENTORY   --
+----------------+---------------
+-- https://www.hackerrank.com/challenges/harry-potter-and-wands --
+
+/* The tables WANDS and WANDS_PROPERTY are described as follows:
+
+The id is the id of the wand, code is the code of the wand, 
+coins_needed is the total number of gold galleons needed to buy the wand, 
+and power denotes the quality of the wand (the higher the power, the better the wand is).
++--------------+-------------+
+|            WANDS           |
++--------------+-------------+
+|  Column      |    Type     |
++--------------+-------------+
+| id           | Integer     |
+| code         | Integer     |
+| coins_needed | Integer     |
+| power        | Integer     |
++--------------+-------------+
+
+The code is the code of the wand, age is the age of the wand, and is_evil denotes whether the wand is good for the dark arts. 
+If the value of is_evil is 0, it means that the wand is not evil. 
+The mapping between code and age is one-one, meaning that if there are two pairs, (code1, age1) and (code2, age2), then code1 =/= code2 and age1 =/= age2.
++---------------+-------------+
+|       WANDS_PROPERTY        |
++---------------+-------------+
+|    Column     |    Type     |
++---------------+-------------+
+| code          |   Integer   |
+| age           |   Integer   |
+| is_evil       |   Integer   |
++---------------+-------------+
+
+
+Harry Potter and his friends are at Ollivander's with Ron, finally replacing Charlie's old broken wand.
+
+Hermione decides the best way to choose is by determining the minimum number of gold galleons needed to buy each non-evil wand of high power and age. 
+Write a query to print the id, age, coins_needed, and power of the wands that Ron's interested in, sorted in order of descending power. 
+If more than one wand has same power, sort the result in order of descending age.
+*/
+
+-- In other words, I'm looking for the cheapest wand for a given combination of age and power.
+
+-- Resubmission using WHERE to filter for only the least expensive wand for each age/power combo
+SELECT w.id, p.age, w.coins_needed, w.power
+FROM WANDS w
+JOIN WANDS_PROPERTY p
+ON w.code = p.code
+WHERE p.is_evil = 0 AND coins_needed = ( -- Queries other wands with same power/age for the least expensive one
+    SELECT MIN(coins_needed) 
+    FROM WANDS w1 
+    JOIN WANDS_PROPERTY p1 
+    ON w1.code = p1.code 
+    WHERE w1.power = w.power AND p1.age = p.age) -- Takes the power/age of the "current"/given wand from the outer query
+ORDER BY w.power DESC, p.age DESC;
+
+-- First submission: left joined a queried table of all the least expensive wands for each power/age combo with the wands_data table to obtain the required info
+SELECT wands_data.id, min_coin_needed.age, min_coin_needed.min_coins, min_coin_needed.power
+FROM (SELECT w1.power, p1.age, MIN(coins_needed) AS min_coins
+FROM WANDS w1
+JOIN WANDS_PROPERTY p1
+ON w1.code = p1.code
+WHERE p1.is_evil = 0
+GROUP BY w1.power, p1.age) AS min_coin_needed
+LEFT JOIN (SELECT w.id, p.age, w.coins_needed, w.power
+FROM WANDS w
+JOIN WANDS_PROPERTY p
+ON w.code = p.code) AS wands_data
+ON wands_data.power = min_coin_needed.power AND wands_data.age = min_coin_needed.age AND wands_data.coins_needed = min_coin_needed.min_coins
+ORDER BY min_coin_needed.power DESC, min_coin_needed.age DESC;
+
+
+
+
